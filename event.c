c18cfe91ff37fb61ff8fb5807e2d7806290c19d6
Lee <peteralfredlee@gmail.com>
2019-11-06 17:05:43 +0800

    test1

--- event.c
+++ event.c
@@ -694,10 +694,7 @@ event_base_new_with_config(const struct event_config *cfg)
 
 		base->evbase = base->evsel->init(base);
 	}
-
-	printf("base->evsel->name:%s\n",base->evsel->name);
 	if (base->evbase == NULL) {
-		printf("======\n");
 		event_warnx("%s: no event mechanism available",
 		    __func__);
 		base->evsel = NULL;
